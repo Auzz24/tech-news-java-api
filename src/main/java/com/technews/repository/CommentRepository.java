f78ebf056a70147236f413b293f2d9b8d6734300
Austin Miller <austin.miller1424@gmail.com>
2022-06-30 16:13:32 -0500

    added vote repository and countVotesByPOstId method

--- src/main/java/com/technews/repository/CommentRepository.java
+++ src/main/java/com/technews/repository/CommentRepository.java
@@ -4,6 +4,9 @@ import com.technews.model.Comment;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.*;
 
+import java.util.*;
+
 @Repository
 public interface CommentRepository extends JpaRepository <Comment, Integer> {
+    List<Comment> findAllCommentsByPOstId(int postId);
 }
