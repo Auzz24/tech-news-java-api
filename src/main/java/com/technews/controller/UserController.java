9123de029abae08af25024f2b426b1fc5e87fedf
Austin Miller <austin.miller1424@gmail.com>
2022-07-01 11:37:10 -0500

    added CRUD routes to PostController

--- src/main/java/com/technews/controller/UserController.java
+++ src/main/java/com/technews/controller/UserController.java
@@ -1,13 +1,12 @@
 package com.technews.controller;
 import com.technews.model.Post;
 import com.technews.model.User;
-import com.technews.model.Vote;
-import com.technews.repository.PostRepository;
 import com.technews.repository.UserRepository;
 import com.technews.repository.VoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 @RestController
